21862ca3d9dd8835610ff4abbe9d5b5aab6649bf
Neelam <pariharneelam376@gmail.com>
2024-03-10 23:25:45 +0530

    jspractice

--- week-2/01-async-js/easy/counter4.js
+++ week-2/01-async-js/easy/counter4.js
@@ -1,3 +1,13 @@
 // ## Write to a file
 // Using the fs library again, try to write to the contents of a file.
 // You can use the fs library to as a black box, the goal is to understand async tasks.
+
+const fs = require("fs");
+const data = " I am changing file content";
+fs.writeFile("textfile.txt", data, (err) => {
+  if (err) {
+    throw new Error("Incorrect file path");
+  }
+  const readnewfile = fs.readFileSync("textfile.txt", "utf-8");
+  console.log(readnewfile);
+});
